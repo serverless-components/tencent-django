706c88145c427b7de11e9e3ee9b17d01f6f85b1c
Avril Li <linxforever@vip.qq.com>
2020-07-01 15:23:55 +0800

    chore: update test case

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,7 +1,7 @@
 const { generateId, getServerlessSdk } = require('./utils')
 
 // set enough timeout for deployment to finish
-jest.setTimeout(30000)
+jest.setTimeout(300000)
 
 // the yaml file we're testing against
 const instanceYaml = {
@@ -11,8 +11,9 @@ const instanceYaml = {
   name: `django-integration-tests-${generateId()}`,
   stage: 'dev',
   inputs: {
-    djangoProjectName: 'djangotest'
-    // region: 'ap-guangzhou'
+    djangoProjectName: 'djangotest',
+    region: 'ap-guangzhou',
+    apigatewayConf: { environment: 'test' }
   }
 }
 
@@ -24,48 +25,19 @@ const credentials = {
 // get serverless construct sdk
 const sdk = getServerlessSdk(instanceYaml.org)
 
-// clean up the instance after tests
-afterAll(async () => {
-  await sdk.remove(instanceYaml, credentials)
-})
-
 it('should successfully deploy django app', async () => {
   const instance = await sdk.deploy(instanceYaml, { tencent: {} })
   expect(instance).toBeDefined()
   expect(instance.instanceName).toEqual(instanceYaml.name)
-})
-
-it('should successfully update basic configuration', async () => {
-  instanceYaml.inputs.region = 'ap-shanghai'
-
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
   expect(instance.outputs).toBeDefined()
   expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
-})
-
-it('should successfully update apigatewayConf', async () => {
-  instanceYaml.inputs.apigatewayConf = { environment: 'test' }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.apigw).toBeDefined()
   expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
-
-})
-
-it('should successfully disable auto create api gateway', async () => {
-  instanceYaml.inputs.apigatewayConf = { isDisabled: true }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
-  expect(instance.outputs.apigw).not.toBeDefined()
 })
 
 it('should successfully remove django app', async () => {
   await sdk.remove(instanceYaml, credentials)
   result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
 
-  // remove action won't delete the service cause the apigw have the api binded
   expect(result.instance.instanceStatus).toEqual('inactive')
 })
